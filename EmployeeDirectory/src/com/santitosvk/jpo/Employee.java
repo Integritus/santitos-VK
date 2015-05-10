818ec404616ef281b428745ddf7c7f12c080f0a6
Daniel Perez de Leon <integritus@gmail.com>
2015-05-09 20:07:19 -0500

    First Commit from exercise.

---
+++ EmployeeDirectory/src/com/santitosvk/jpo/Employee.java
@@ -0,0 +1,87 @@
+package com.santitosvk.jpo;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import org.hibernate.validator.constraints.Email;
+
+
+
+/**
+ * The persistent class for the EMPLOYEE database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Employee.findAll", query="SELECT e FROM Employee e")
+public class Employee implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	@Email
+	private String email;
+
+	@Column(name="JOB_TITLE")
+	private String jobTitle;
+
+	private String name;
+
+	private String phoneNumber;
+	
+	public String getCellphoneNumber() {
+		return cellphoneNumber;
+	}
+
+	public void setCellphoneNumber(String cellphoneNumber) {
+		this.cellphoneNumber = cellphoneNumber;
+	}
+
+	private String cellphoneNumber;
+
+	public Employee() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getJobTitle() {
+		return this.jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
